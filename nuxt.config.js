6458aabf379c91efe62cd47f8f759cbd577b235b
Erick Hilda <erick.hilda@ajaib.co.id>
2022-05-15 18:06:50 +0700

    feat: add generate property on nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ const websocketUrl =
 
 export default {
   target: 'static',
+  generate: {
+    fallback: true,
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'alta-nuxt',
